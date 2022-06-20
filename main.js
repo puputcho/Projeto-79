e5e48adea4f90919593330d9465ab45f27b0bc57
puputcho <67434177+puputcho@users.noreply.github.com>
2022-06-20 18:42:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+menuListArray = ["Pizza Vegetariana","Pizza de Frango", "Pizza Portuguesa", "Pizza Quatro Queijos", "Pizza de Calabresa","Pizza de Pizza"];
+
+function getMenu(){
+htmldata = ""
+menuListArray.sort()
+for (var i=0;  i< menuListArray.length; i++) {
+    htmldata=htmldata+imgtags+menuListArray[i]+ '<br>';
+}
+}
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/> '
+var item=document.getElementById("addItem").value;
+menuListArray.sort()
+htmldata=""
+for (var i=0;  i< menuListArray.length; i++) {
+    htmldata=htmldata+imgtags+menuListArray[i]+ '<br>';
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+}
+
+function addTop(){
+var item=document.getElementById("addItem").value
+menuListArray.push(item)
+addItem()
+}
